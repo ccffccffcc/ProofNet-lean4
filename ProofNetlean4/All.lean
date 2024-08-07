847dee7cc7a4bebb99f7cee7f916274e718a2a29
feng.chen <feng.chen@bytedance.com>
2024-08-07 00:04:44 +0000

    update

--- ProofNetlean4/All.lean
+++ ProofNetlean4/All.lean
@@ -1,9 +1,9 @@
 import ProofNetlean4.Artin
 import ProofNetlean4.Axler
-import ProofNetlean4.Cambridge-Tripos
-import ProofNetlean4.Dummit-Foote
+import ProofNetlean4.CambridgeTripos
+import ProofNetlean4.DummitFoote
 import ProofNetlean4.Herstein
-import ProofNetlean4.Ireland-Rosen
+import ProofNetlean4.IrelandRosen
 import ProofNetlean4.Munkers
 import ProofNetlean4.Pough
 import ProofNetlean4.Putnam
